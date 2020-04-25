b56a21aa86ef8cd317b520f182e6397e2a6c00fe
Pavel Filippov <zanziver@gmail.com>
2020-04-25 14:01:22 +0200

    Добавляем обработчик кликов для списка слева, пока пустой

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 
 const onLoad = () => {
   showFirstCard();
+  attachClicksToList();
 }
 
 const showFirstCard = () => {
   document.getElementById('card-1').style.display = 'block';
+}
+
+const attachClicksToList = () => {
+  const nodeList = document.getElementById('topicsList').children; // children лучше, чем nodeList, потому что не приносят TextNodes, а только тэги
+  const nodeListArray = Array.from(nodeList); //  Превращаем HTMLCollection в массив, чтобы использовать forEach
+
+  nodeListArray.forEach((item, i) => {
+    item.addEventListener('click', clickOnTopic)
+  });
+}
+
+const clickOnTopic = (event) => {
 }
 
 document.addEventListener("DOMContentLoaded", onLoad);
